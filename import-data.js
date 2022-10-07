57a0853db39615e9885441c56d0e7b1a9f053571
David TEIXEIRA <david.teixeira2@edu.devinci.fr>
2022-10-07 17:37:52 +0200

    TD2_07/10

--- import-data.js
+++ import-data.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 //console.log(process.env.MONGO_URI)
 const mongoose= require('mongoose')
+const filmingLocations = require('./lieux-de-tournage-a-paris.json')
 const { Schema } = mongoose
 const Locations = new Schema({
     filmType:  String,
@@ -25,12 +26,30 @@ const Locations = new Schema({
 //mongoose.connect(process.env.MONGO_URI).then(()=>{
   //  console.log(('Connected !'))})
 //console.log('toto')
+
+async function jsonToMongo (filmingLocations) {
+    const Location = new mongoose.model('Location',Locations)
+    for(let filmingLocation of filmingLocations){
+        const maLocation = new Location({
+            filmType:  filmingLocation.fields.type_tournage,
+            filmProducerName: filmingLocation.fields.nom_producteur,
+            endDate:new Date (filmingLocation.fields.date_fin),
+            district: filmingLocation.fields.ardt_lieu,
+            geolocation: filmingLocation.fields.geo_shape,
+            sourceLocationId: filmingLocation.fields.id_lieu,
+            filmDirectorName: filmingLocation.fields.nom_realisateur,
+            address: filmingLocation.fields.address_lieu,
+            startDate: new Date(filmingLocation.fields.date_debut),
+            year: Number(filmingLocation.fields.annee_tournage),
+        })
+        await maLocation.save()
+    }
+}
 async function main(){
-        const result = await mongoose.connect(process.env.MONGO_URI)
-        //const result = await Promise.all([mongoose.connect(1),mongoose.connect(2)])
-        console.log('connected')
-        const Location = new mongoose.model('Location',Locations)
-        const maPremiereLocation = new Location({filmType:'Horror'})
-        await maPremiereLocation.save()
+    const result = await mongoose.connect(process.env.MONGO_URI)
+    //const result = await Promise.all([mongoose.connect(1),mongoose.connect(2)])
+    console.log('connected')
+    //await jsonToMongo(filmingLocations)
+    console.log('done')
 }
 main()

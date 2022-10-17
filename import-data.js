335e91a3307ffbdb8554c211eb210d35faa41736
David TEIXEIRA <david.teixeira2@edu.devinci.fr>
2022-10-17 16:52:00 +0200

    TD2_17/10

--- import-data.js
+++ import-data.js
@@ -7,6 +7,7 @@ const Locations = new Schema({
     filmType:  String,
     filmProducerName: String,
     endDate:   Date,
+    filmName: String,
     district: String,
     geolocation:{
         coordinates:{
@@ -23,6 +24,7 @@ const Locations = new Schema({
     startDate: Date,
     year: Number,
 })
+const Location = mongoose.model('Location',Locations)
 //mongoose.connect(process.env.MONGO_URI).then(()=>{
   //  console.log(('Connected !'))})
 //console.log('toto')
@@ -34,6 +36,7 @@ async function jsonToMongo (filmingLocations) {
             filmType:  filmingLocation.fields.type_tournage,
             filmProducerName: filmingLocation.fields.nom_producteur,
             endDate:new Date (filmingLocation.fields.date_fin),
+            filmName: filmingLocation.fields.nom_tournage,
             district: filmingLocation.fields.ardt_lieu,
             geolocation: filmingLocation.fields.geo_shape,
             sourceLocationId: filmingLocation.fields.id_lieu,
@@ -45,11 +48,42 @@ async function jsonToMongo (filmingLocations) {
         await maLocation.save()
     }
 }
+function queryId(id) {
+    Location.findById(id).then(film => console.log(film))
+}
+
+function queryFilmName(nameOfFilm) {
+    Location.find({filmName : nameOfFilm}).then(films => films.forEach(film => console.log(film)))
+}
+
+function deleteId(id) {
+    Location.findOneAndDelete({_id:id}).then(console.log('Deleted !'))
+}
+
+function addLocation(location) {
+    try{
+        location.save();
+    } catch (e) {
+        console.log("Something went wrong !")
+    }
+}
+
+function updateLocation(id,parameter,update){
+    //Location.update({_id:id},{filmProducerName :update})
+    Location.updateOne({_id: id}, {$set:{parameter:update}}).then(console.log('done'))
+}
+
 async function main(){
     const result = await mongoose.connect(process.env.MONGO_URI)
     //const result = await Promise.all([mongoose.connect(1),mongoose.connect(2)])
     console.log('connected')
     //await jsonToMongo(filmingLocations)
+    //await queryId("633f1ddb5968cf5f5f8f5654")
+    //await addLocation(new Location ({filmName : "David c'est moi"}))
+    //await deleteId("634d61eb8db12ad6cd8a38c2")
+    //await queryFilmName("David c'est moi")
+    await updateLocation("634d61fa92ffb8fe9e907423",'filmProducerName','David3')
+    await queryId("634d61fa92ffb8fe9e907423")
     console.log('done')
 }
 main()
